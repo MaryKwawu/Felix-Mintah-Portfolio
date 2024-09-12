53ebb435241b8e95f988f1d36f8cf0048c828add
MaryKwawu <may419316@gmail.com>
2024-09-12 14:25:17 +0000

    Chore : worked on the stats.tsx file

--- src/components/ui/stats.tsx
+++ src/components/ui/stats.tsx
@@ -24,7 +24,7 @@ const stats = [
 
 const Stats = () => {
 	return (
-		<section>
+		<section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
 			<div className="container mx-auto">
 				<div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
 					{stats.map((item, index) => {
@@ -41,7 +41,7 @@ const Stats = () => {
 								<p
 									className={`${
 										item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
-									}`}>
+									} leading-snug text-white/80`}>
 									{item.text}
 								</p>
 							</div>
