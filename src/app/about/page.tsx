9104d2794d6a542ed2a23e5efb2bae7c1ce716e1
MaryKwawu <may419316@gmail.com>
2024-09-24 16:08:47 +0000

    fix/about : fixed a with respect to personalDetails

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,133 +2,134 @@
 import { Description } from "@radix-ui/react-dialog";
 import React from "react";
 
+import {
+	SiPowerbi,
+	SiMicrosoftexcel,
+	SiVisualbasic,
+	SiPowerapps,
+	SiGoogleanalytics,
+} from "react-icons/si";
 
 //about data
-const personalDetails = [
-	{
-		title: "Personal Details",
-		Description:
-			"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem dolorem provident rerum",
-		info: [
-			{
-				fieldName: "Name",
-				fieldValue: "Felix Mintah",
-			},
-			{
-				fieldName: "Phone",
-				fieldValue: "(+233 000 0000)",
-			},
-			{
-				fieldName: "Experience",
-				fieldValue: "12+ Years",
-			},
-			{
-				fieldName: "Nationality",
-				fieldValue: "Ghanaian",
-			},
-			{
-				fieldName: "Email",
-				fieldValue: "felix@gmail.com",
-			},
-			{
-				fieldName: "Contracts",
-				fieldValue: "Available",
-			},
-			{
-				fieldName: "Language",
-				fieldValue: "English, Twi & Ga",
-			},
-		],
-	},
-];
+const personalDetails = {
+	title: "Personal Details",
+	Description:
+		"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem dolorem provident rerum",
+	info: [
+		{
+			fieldName: "Name",
+			fieldValue: "Felix Mintah",
+		},
+		{
+			fieldName: "Phone",
+			fieldValue: "(+233 000 0000)",
+		},
+		{
+			fieldName: "Experience",
+			fieldValue: "12+ Years",
+		},
+		{
+			fieldName: "Nationality",
+			fieldValue: "Ghanaian",
+		},
+		{
+			fieldName: "Email",
+			fieldValue: "felix@gmail.com",
+		},
+		{
+			fieldName: "Contracts",
+			fieldValue: "Available",
+		},
+		{
+			fieldName: "Language",
+			fieldValue: "English, Twi & Ga",
+		},
+	],
+};
 
 //experience data
-const experience = [
-	{
-		title: "My experience",
-		Description:
-			"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem dolorem provident rerum",
-		item: [
-			{
-				company: "Unilever",
-				position: "Customer Development IT Manager",
-				duration: "Oct 2023 - Present",
-			},
-			{
-				company: "Territory Manager",
-				position: "Customer Development IT Manager",
-				duration: "Oct 2021 - Oct 2023",
-			},
-			{
-				company: "Unilever",
-				position: "Management Informationn Specialis",
-				duration: "July 2020 - Sep 2021",
-			},
-			{
-				company: "Unilever",
-				position: "Assistant Territory Manager",
-				duration: "Oct 2019 - July 2020",
-			},
-			{
-				company: "Intercity STC Coaches Limited ",
-				position: "Welding and Fabrication Intern",
-				duration: "Jun 2018 - Aug 2018",
-			},
-			{
-				company: "Unilever",
-				position: "Plant Production Technician",
-				duration: "Feb 2016 - Sep 2016",
-			},
-		],
-	},
-];
+const experience = {
+	title: "My experience",
+	Description:
+		"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem dolorem provident rerum",
+	items: [
+		{
+			company: "Unilever",
+			position: "Customer Development IT Manager",
+			duration: "Oct 2023 - Present",
+		},
+		{
+			company: "Territory Manager",
+			position: "Customer Development IT Manager",
+			duration: "Oct 2021 - Oct 2023",
+		},
+		{
+			company: "Unilever",
+			position: "Management Informationn Specialis",
+			duration: "July 2020 - Sep 2021",
+		},
+		{
+			company: "Unilever",
+			position: "Assistant Territory Manager",
+			duration: "Oct 2019 - July 2020",
+		},
+		{
+			company: "Intercity STC Coaches Limited ",
+			position: "Welding and Fabrication Intern",
+			duration: "Jun 2018 - Aug 2018",
+		},
+		{
+			company: "Unilever",
+			position: "Plant Production Technician",
+			duration: "Feb 2016 - Sep 2016",
+		},
+	],
+};
 
 //education data
-const education = [
-	{
-		title: "My education",
-		Description:
-			"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem dolorem provident rerum",
-		item: [
-			{
-				institution: "Accra Technical University",
-				degree: "Bachelor of Technology in Mechanical Engineering",
-				duration: "Sep 2021 - Feb 2023",
-			},
-			{
-				institution: "Accra Technical University",
-				degree: "Higher Diploma in Plant Engineering",
-				duration: "Sep 2016 - Dec 2019",
-			},
-			{
-				institution: "Inbound Sales",
-				degree: "HubSpot Acadamy",
-				duration: "April 2023",
-			},
-			{
-				institution: "University of Colorado Boulder",
-				degree: "Initiating & Planning Project",
-				duration: "Oct 2022",
-			},
-			{
-				institution:
-					"University of Califonia Irvine Division of Continuing Education",
-				degree: "Excel/Vba For Creative Problem",
-				duration: "Oct 2022",
-			},
-			{
-				institution: "LinkedIn",
-				degree: "Power BI Essential Training",
-				duration: "May 2021",
-			},
-			{
-				institution: "LinkedIn",
-				degree: "Data Dashboard in Power BI",
-				duration: "May 2021",
-			},
-		],
-	},
-];
+const education = {
+	title: "My education",
+	Description:
+		"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem dolorem provident rerum",
+	item: [
+		{
+			institution: "Accra Technical University",
+			degree: "Bachelor of Technology in Mechanical Engineering",
+			duration: "Sep 2021 - Feb 2023",
+		},
+		{
+			institution: "Accra Technical University",
+			degree: "Higher Diploma in Plant Engineering",
+			duration: "Sep 2016 - Dec 2019",
+		},
+		{
+			institution: "Inbound Sales",
+			degree: "HubSpot Acadamy",
+			duration: "April 2023",
+		},
+		{
+			institution: "University of Colorado Boulder",
+			degree: "Initiating & Planning Project",
+			duration: "Oct 2022",
+		},
+		{
+			institution:
+				"University of Califonia Irvine Division of Continuing Education",
+			degree: "Excel/Vba For Creative Problem",
+			duration: "Oct 2022",
+		},
+		{
+			institution: "LinkedIn",
+			degree: "Power BI Essential Training",
+			duration: "May 2021",
+		},
+		{
+			institution: "LinkedIn",
+			degree: "Data Dashboard in Power BI",
+			duration: "May 2021",
+		},
+	],
+};
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
@@ -159,19 +160,36 @@ const About = () => {
 					<TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
 						<TabsTrigger value="personal details">Personal Details</TabsTrigger>
 						<TabsTrigger value="experience">Experience</TabsTrigger>
-						<TabsTrigger value="educationn">Education</TabsTrigger>
+						<TabsTrigger value="education">Education</TabsTrigger>
 					</TabsList>
 					{/* content */}
 					<div className="min-h-[70vh] w-full">
 						{/* personal details */}
 						<TabsContent value="personal details" className="w-full">
-							<div>
-								<h3>{personalDetails.}</h3>
+							<div className="flex flex-col gap-[30px] text-center xl:text-left">
+								<h3 className="text-4xl font-bold">{personalDetails.title}</h3>
+								<p className="max-w-[600] text-white/60 mx-auto xl:mx-0">
+									{personalDetails.Description}
+								</p>
+								<ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+									{personalDetails.info.map((item, index) => {
+										return (
+											<div key={index}>
+												<li className="flex items-center justify-center xl:justify-start gap-4">
+													<span className="text-white/60">
+														{item.fieldName}
+													</span>
+													<span className="text-xl">{item.fieldValue}</span>
+												</li>
+											</div>
+										);
+									})}
+								</ul>
 							</div>
 						</TabsContent>
 						{/* experience */}
 						<TabsContent value="experience" className="w-full">
-							<h3></h3>
+							experience
 						</TabsContent>
 						{/* education */}
 						<TabsContent value="education" className="w-full">
