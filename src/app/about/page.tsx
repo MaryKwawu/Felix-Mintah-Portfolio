37e40d11785536b596467133664e25ba530bf78b
MaryKwawu <may419316@gmail.com>
2024-09-25 07:15:41 +0000

    feat/about : worked on the experience feature

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -141,6 +141,7 @@ import {
 
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { motion } from "framer-motion";
+import { Item } from "@radix-ui/react-select";
 
 const About = () => {
 	return (
@@ -189,11 +190,73 @@ const About = () => {
 						</TabsContent>
 						{/* experience */}
 						<TabsContent value="experience" className="w-full">
-							experience
+							{/* experience */}
+							<div className="flex flex-col gap-[30px] text-center xl:text-left">
+								<h3 className="text-4xl font-bold">{experience.title}</h3>
+								<p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+									{experience.Description}
+								</p>
+								<ScrollArea className="h-[400px]">
+									<ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+										{experience.items.map((Item, index) => {
+											return (
+												<div
+													key={index}
+													className="bg-[#c4cec3] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+												>
+													<li>
+														<span className="text-accent">{Item.duration}</span>
+														<h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+															{Item.position}
+														</h3>
+														<div className="flex items-center gap-3">
+															{/* dot */}
+															<span className="w-[6px] h-[6px] rounded-full bg-accent">
+																{}
+															</span>
+															<p className="text-white/60">{Item.company}</p>
+														</div>
+													</li>
+												</div>
+											);
+										})}
+									</ul>
+								</ScrollArea>
+							</div>
 						</TabsContent>
 						{/* education */}
 						<TabsContent value="education" className="w-full">
-							education
+							<div className="flex flex-col gap-[30px] text-center xl:text-left">
+								<h3 className="text-4xl font-bold">{experience.title}</h3>
+								<p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+									{experience.Description}
+								</p>
+								<ScrollArea className="h-[400px]">
+									<ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+										{experience.items.map((Item, index) => {
+											return (
+												<div
+													key={index}
+													className="bg-[#c4cec3] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+												>
+													<li>
+														<span className="text-accent">{Item.duration}</span>
+														<h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+															{Item.position}
+														</h3>
+														<div className="flex items-center gap-3">
+															{/* dot */}
+															<span className="w-[6px] h-[6px] rounded-full bg-accent">
+																{}
+															</span>
+															<p className="text-white/60">{Item.company}</p>
+														</div>
+													</li>
+												</div>
+											);
+										})}
+									</ul>
+								</ScrollArea>
 						</TabsContent>
 					</div>
 				</Tabs>
